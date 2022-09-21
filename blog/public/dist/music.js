5b65da6e29c0429569e5918647c28c257115aa4f
jsonli <2464113103@qq.com>
2022-09-21 23:25:25 +0800

    public: 发布-搜索样式调整

---
+++ blog/public/dist/music.js
@@ -0,0 +1,19 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "一生只想跟你走",
+        artist: '张学友',
+        url: 'https://www.xzmp3.com/down/77f0b34e1432.mp3',
+        cover: 'https://img1.imgtp.com/2022/09/18/ykxBDTnB.png',
+      },
+      {
+        name: 'China-X',
+        artist: '徐梦圆',
+        url: 'https://www.ytmp3.cn/down/49003.mp3',
+        cover: 'https://img1.imgtp.com/2022/09/18/ykxBDTnB.png',
+      },
+    ]
+});
